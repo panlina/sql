cf504af8b8970ad81fea96d1499791a76230b194
Jack Lu <jacklu@jacklu.me>
2019-07-05 17:22:38 +0800

    +where

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ function generate(sql) {
 			var select = `select ${sql.field.map(generate).join(',')}`;
 			if (sql.from)
 				select += ` from (${generate(sql.from)})`;
+			if (sql.where)
+				select += ` where (${generate(sql.where)})`;
 			return select;
 		case 'binary':
 			return `(${generate(sql.left)})${sql.operator}(${generate(sql.right)})`;
