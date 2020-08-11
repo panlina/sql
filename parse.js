cf08638d5fd11ee97c2cf16859ae5213a59a3605
Jack Lu <jacklu@jacklu.me>
2020-08-11 23:52:30 +0800

    parse: -startRule

--- parse.js
+++ parse.js
@@ -1,8 +1,8 @@
 var grammar = require('./grammar');
 var semantics = require('./semantics');
 var ParseError = require('./ParseError');
-module.exports = (text, startRule) => {
-	var matchResult = grammar.match(text, startRule);
+module.exports = text => {
+	var matchResult = grammar.match(text);
 	if (matchResult.failed()) throw new ParseError(matchResult);
 	return semantics(matchResult).parse();
 }
