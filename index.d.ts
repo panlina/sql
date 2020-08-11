4c926a64c9375d2fae6775065b4d6659f81f2854
Jack Lu <jacklu@jacklu.me>
2020-08-11 23:59:31 +0800

    +sql.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,4 @@
+import * as sql from './sql';
+export = sql;
 export * as Expression from './Expression';
 export * as parse from './parse';
---
+++ sql.d.ts
@@ -0,0 +1,3 @@
+import * as Expression from "./Expression";
+function sql(strings: string[], ...expressions: Expression[]): Expression;
+export = sql;
